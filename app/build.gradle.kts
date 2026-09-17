41cbf66f43c5353258dfc5dc8144a3c069241be2
Szymon <szymon.osinski.2009@gmail.com>
2026-09-17 17:53:21 +0200

    chore: setup github; build.gradle.kts: debug buildType

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
